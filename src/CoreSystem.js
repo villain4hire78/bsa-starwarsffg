1f53ad6a998d4ab96ccfb433e8f43e2f7800a0b6
Villain4Hire <roninjedi78@gmail.com>
2025-06-08 23:08:54 -0400

    Fix: Actually add configLootItemType method content

--- src/CoreSystem.js
+++ src/CoreSystem.js
@@ -6,4 +6,12 @@
       priceProperty: "price"
     };
   }
+
+  get configLootItemType() {
+    return {
+      itemType: "gear",
+      quantityProperty: "quantity", 
+      priceProperty: "price"
+    };
+  }
 }
